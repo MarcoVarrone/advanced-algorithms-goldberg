ce874a23778c16c5e061d3c5aa9055cd8e619c04
MarcoVarrone <mrc.varrone@gmail.com>
2018-09-19 19:28:44 +0200

    Remove private function declaration and remove set_excess() function

--- Golberg.py
+++ Golberg.py
@@ -25,23 +25,20 @@ class Goldberg:
 
         for edge in source_edges:
             self.flow[edge] = self.capacity[edge]
-            self.__set_excess(edge.target(), self.capacity[edge])
+            self.excess[edge.target()] = self.capacity[edge]
 
-        active = self.__get_active_vertex(sink)
+        active = self.get_active_vertex(sink)
         while active:
-            if not self.__push(active):
-                self.__relabel(active)
-            active = self.__get_active_vertex(sink)
+            if not self.push(active):
+                self.relabel(active)
+            active = self.get_active_vertex(sink)
 
         max_flow = 0
         for e in sink.in_edges():
             max_flow += self.flow[e]
         return max_flow
 
-    def __set_excess(self, vertex, value):
-        self.excess[vertex] = value
-
-    def __push(self, vertex):
+    def push(self, vertex):
         success = False
         out_edges = vertex.out_edges()
 
@@ -53,16 +50,16 @@ class Goldberg:
             delta = min([
                 self.excess[vertex],
                 self.capacity[edge] - self.flow[edge]])
-            self.__send_flow(edge.source(), edge.target(), delta)
+            self.send_flow(edge.source(), edge.target(), delta)
             print("Pushing " + str(delta) + " from " + str(edge.source()) + " to " + str(edge.target()))
 
         return success
 
-    def __relabel(self, vertex):
-        self.distance[vertex] = self.__get_min_distance(vertex) + 1
+    def relabel(self, vertex):
+        self.distance[vertex] = self.get_min_distance(vertex) + 1
         print("Relabeling " + str(vertex) + " to dist " + str(self.distance[vertex]))
 
-    def __get_min_distance(self, vertex):
+    def get_min_distance(self, vertex):
         min = float('inf')
         for e in vertex.out_edges():
             if self.flow[e] == self.capacity[e]:
@@ -73,17 +70,17 @@ class Goldberg:
                 min = self.distance[v]
         return min
 
-    def __send_flow(self, source, target, delta):
+    def send_flow(self, source, target, delta):
         self.flow[self.graph.edge(source, target)] += delta
         self.flow[self.graph.edge(target, source)] -= delta
 
-        self.__set_excess(source, self.excess[source] - delta)
-        self.__set_excess(target, self.excess[target] + delta)
+        self.excess[source] -= delta
+        self.excess[target] += delta
 
-    def __get_reverse_edge(self, edge):
+    def get_reverse_edge(self, edge):
         return self.graph.edge(edge.target(), edge.source())
 
-    def __get_active_vertex(self, sink):
+    def get_active_vertex(self, sink):
         for v in self.graph.vertices():
             if self.excess[v] > 0 and v != sink:
                 return v
