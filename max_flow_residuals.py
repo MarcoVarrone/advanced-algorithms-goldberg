06a70a9379f9e4bf0dcb464559c4364b85953619
MarcoVarrone <mrc.varrone@gmail.com>
2018-09-25 15:29:03 +0200

    Use active list for Push Relabel using residuals

--- max_flow_residuals.py
+++ max_flow_residuals.py
@@ -13,14 +13,12 @@ class Goldberg:
 
         self.height = self.graph.new_vertex_property("int", 0)
         self.excess = self.graph.new_vertex_property("int", 0)
-        self.flow = self.graph.new_edge_property("int", 0)
         self.res = self.graph.new_edge_property("int", 0)
         self.real = self.graph.new_edge_property("bool", True)
         self.capacity = self.graph.ep.cap
 
         self.graph.vp.height = self.height
         self.graph.vp.excess = self.excess
-        self.graph.ep.flow = self.flow
         self.actives = set()
 
         self.i = 0
@@ -29,7 +27,7 @@ class Goldberg:
         self.source = source
         self.sink = sink
         self.init(source)
-        self.__print_residuals(self.i)
+        #self.__print_residuals()
 
         active = self.get_active_vertex()
         while active:
@@ -39,31 +37,30 @@ class Goldberg:
 
         return self.excess[sink]
 
-    def __print_residuals(self, index):
+    def __print_residuals(self):
         labels = self.graph.new_vertex_property("string")
         for vertex in self.graph.vertices():
             labels[vertex] = str(self.graph.vertex_index[vertex]) + "(" + str(self.height[vertex]) + "," + str(
                 self.excess[vertex]) + ")"
 
-        #gt.graph_draw(self.graph, edge_pen_width=gt.prop_to_size(self.res, mi=1, ma=5, power=1),
-        #              output="cf/cf_residuals" + str(index) + ".pdf", vertex_text=labels, edge_text=self.res)
-        print("Print graph "+str(index))
+        gt.graph_draw(self.graph, edge_pen_width=gt.prop_to_size(self.res, mi=1, ma=5, power=1),
+                      output="cf/cf_residuals" + str(self.i) + ".pdf", vertex_text=labels, edge_text=self.res)
+        print("Print graph "+str(self.i))
         self.i += 1
 
     def push(self, vertex):
         success = False
-        print("Chosen vertex:" + str(vertex))
+        #print("Chosen vertex:" + str(vertex))
         for edge in list(vertex.out_edges()):
-            print("Edge:"+str(edge))
-            # ToDO: check other conditions
+            #print("Edge:"+str(edge))
             if self.height[edge.source()] != self.height[edge.target()] + 1:
                 continue
 
             success = True
             delta = min(self.excess[vertex], self.res[edge])
             self.send_flow(vertex, edge.target(), delta)
-            print("Pushing "+str(delta)+" along "+str(edge))
-            self.__print_residuals(self.i)
+            #print("Pushing "+str(delta)+" along "+str(edge))
+            #self.__print_residuals()
 
             if self.excess[vertex] == 0:
                 break
@@ -71,8 +68,8 @@ class Goldberg:
 
     def relabel(self, vertex):
         self.height[vertex] = self.get_min_distance(vertex) + 1
-        print("Relabeling vertex " + str(vertex) + " to distance " + str(self.height[vertex]))
-        self.__print_residuals(self.i)
+        #print("Relabeling vertex " + str(vertex) + " to distance " + str(self.height[vertex]))
+        #self.__print_residuals()
 
     def get_min_distance(self, vertex):
         min_h = float('inf')
@@ -94,26 +91,40 @@ class Goldberg:
             reverse_edge = self.graph.add_edge(target, source)
             self.real[reverse_edge] = not real
         self.increase_res(reverse_edge, value)
-        self.excess[target] += value
-        self.excess[source] -= value
+        self.set_excess(target, self.excess[target] + value)
+        self.set_excess(source, self.excess[source] - value)
 
     def increase_res(self, edge, value):
         self.res[edge] += value
         if self.res[edge] <= 0:
-            print("Deleted edge " + str(edge))
+            #print("Deleted edge " + str(edge))
             self.graph.remove_edge(edge)
 
     def init(self, source):
         self.height[source] = self.n
         for edge in self.graph.edges():
-            self.res[edge] = self.capacity[edge] - self.flow[edge]
+            self.res[edge] = self.capacity[edge]
 
         for edge in list(source.out_edges()):
-            print("Send flow edge: " + str(edge))
+            #print("Send flow edge: " + str(edge))
             self.send_flow(edge.source(), edge.target(), self.capacity[edge])
 
     def get_active_vertex(self):
-        for vertex in self.graph.vertices():
-            if self.excess[vertex] > 0 and vertex != self.source and vertex != self.sink:
-                return vertex
-        return False
+        if len(self.actives) == 0:
+            return False
+
+        # Get an element from the set without popping it
+        for v in self.actives:
+            return v
+
+    def set_excess(self, vertex, value):
+
+        self.excess[vertex] = value
+        if vertex == self.sink or vertex == self.source:
+            return
+        if value > 0:
+            if vertex not in self.actives:
+                self.actives.add(vertex)
+        else:
+            self.actives.discard(vertex)
+
