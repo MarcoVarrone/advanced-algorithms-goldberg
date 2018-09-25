10d2450a8a9b63993f0820b657006f1d6c4faeb8
MarcoVarrone <mrc.varrone@gmail.com>
2018-09-25 21:10:35 +0200

    Add test to check if source and sink are unique

---
+++ test_source_sinks.py
@@ -0,0 +1,68 @@
+from numpy.random import randint
+import graph_tool.all as gt
+from generation.Triangulation import Triangulation
+from generation.ScaleFree import ScaleFree
+import pytest
+
+
+def has_unique_source(graph):
+    count = 0
+    source = None
+    for vertex in graph.vertices():
+        if vertex.in_degree() == 0:
+            source = vertex
+            count += 1
+    print(count)
+    return count == 1, source
+
+
+def has_unique_sink(graph):
+    count = 0
+    sink = None
+    for vertex in graph.vertices():
+        if vertex.out_degree() == 0:
+            sink = vertex
+            count += 1
+    print(count)
+    return count == 1, sink
+
+
+sizes = [10, 25, 50, 75, 100]
+
+
+@pytest.mark.parametrize('n', sizes)
+def test_source_sink_triangulation_simple(n):
+    seed_number = randint(1, 1000)
+    generator = Triangulation(n, type="simple", directed=True, seed_number=seed_number)
+    graph, source, sink = generator.generate()
+    count_source, src = has_unique_source(graph)
+    count_sink, snk = has_unique_sink(graph)
+    assert count_source == 1
+    assert count_sink == 1
+    assert source == src
+    assert sink == snk
+
+
+@pytest.mark.parametrize('n', sizes)
+def test_source_sink_triangulation_delaunay(n):
+    seed_number = randint(1, 1000)
+    generator = Triangulation(n, type="delaunay", directed=True, seed_number=seed_number)
+    graph, source, sink = generator.generate()
+    count_source, src = has_unique_source(graph)
+    count_sink, snk = has_unique_sink(graph)
+    assert count_source == 1
+    assert count_sink == 1
+    assert source == src
+    assert sink == snk
+
+@pytest.mark.parametrize('n', sizes)
+def test_source_sink_scale_free(n):
+    seed_number = randint(1, 1000)
+    generator = ScaleFree(n, directed=True, seed_number=seed_number)
+    graph, source, sink = generator.generate()
+    count_source, src = has_unique_source(graph)
+    count_sink, snk = has_unique_sink(graph)
+    assert count_source == 1
+    assert count_sink == 1
+    assert source == src
+    assert sink == snk
