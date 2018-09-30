8742450129263433d39991b5319dcc4c19a54cbb
MarcoVarrone <mrc.varrone@gmail.com>
2018-09-30 14:03:02 +0200

    Fix Random graph unique source and sink

--- generation/Random.py
+++ generation/Random.py
@@ -1,5 +1,5 @@
 import graph_tool.all as gt
-from numpy.random import seed, random, randint
+from numpy.random import seed, randint
 from generation.Generator import Generator
 
 
@@ -14,19 +14,47 @@ class Random(Generator):
         if not isinstance(m, int):
             raise ValueError("The number of edges must be an integer.")
 
-    @staticmethod
-    def select_source_target(graph):
-        n = len(graph.get_vertices())
-        source = graph.vertex(randint(0, n - 1))
-        target = None
-        while target is None or source == target:
-            target = graph.vertex(randint(0, n - 1))
-        return source, target
+    def restore_number_edges(self, graph, source, sink, capacity):
+        while len(graph.get_edges()) < self.m:
+            s = randint(0, self.n)
+            t = randint(0, self.n)
+            if s == t or s == sink or t == source:
+                continue
+            if graph.edge(s, t):
+                continue
+
+            edge = graph.add_edge(s, t)
+            # The capacity will be defined randomly
+            cap = randint(1, 15)
+            capacity[edge] = cap
+            if not self.directed:
+                reverse_edge = graph.edge(t, s, add_missing=True)
+                capacity[reverse_edge] = cap
+
+        while len(graph.get_edges()) > self.m:
+            e = randint(0, len(graph.get_edges()))
+            edge = gt.find_edge(graph, graph.edge_index, e)
+            if not edge:
+                continue
+            edge = edge[0]
+            s = edge.source()
+            t = edge.target()
+            if s == t or s == source or t == sink:
+                continue
+
+            if edge:
+                graph.remove_edge(edge)
+
+                if not self.directed:
+                    graph.remove_edge(graph.edge(t, s))
+
+        return graph
 
     def generate(self):
         g = gt.Graph()
         g.add_vertex(self.n)
         capacity = g.new_edge_property("int")
+        g.ep.cap = capacity
         # insert some random links
         for s, t in zip(randint(0, self.n, self.m), randint(0, self.n, self.m)):
             if s == t:
@@ -34,13 +62,15 @@ class Random(Generator):
             if g.edge(s, t):
                 continue
             edge = g.add_edge(s, t)
-            reverse_edge = g.edge(t, s, add_missing=True)
 
             # The capacity will be defined randomly
             cap = randint(1, 15)
             capacity[edge] = cap
-            capacity[reverse_edge] = cap
+            if not self.directed:
+                reverse_edge = g.edge(t, s, add_missing=True)
+                capacity[reverse_edge] = cap
 
-        source, sink = self.select_source_target(g)
-        g.ep.cap = capacity
+        source = super().get_source(g)
+        sink = super().get_sink(g)
+
         return g, source, sink
